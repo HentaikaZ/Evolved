d591114cad62892a32a6caaf248d36185cbf44ae
HentaikaZ <118474408+HentaikaZ@users.noreply.github.com>
2025-01-28 02:15:12 +0300

    Update Evolved.lua

--- Evolved.lua
+++ Evolved.lua
@@ -1,6 +1,7 @@
 os.execute('color 0')
 
 -- Libraries
+require('addon')
 local encoding = require('encoding')
 encoding.default = 'CP1251'
 u8 = encoding.UTF8
@@ -13,6 +14,7 @@ local ffi = require('ffi')
 local socket = require 'socket'
 local inicfg = require('inicfg')
 local cfg = inicfg.load(nil, 'E-Settings')
+
 local configtg = {
     token = cfg.telegram.tokenbot,
     chat_id = cfg.telegram.chatid
@@ -28,34 +30,114 @@ local SPECIAL_KEYS = {
     H = 3
 }
 
+function pressSpecialKey(key)
+    if not SPECIAL_KEYS[key] then return false end
+    specialKey = SPECIAL_KEYS[key]
+    updateSync()
+end
+
+function sampev.onSendPlayerSync(data)
+	if rep then
+		return false
+	end
+	if specialKey then
+		data.specialKey = specialKey
+		specialKey = nil
+	end
+end
+
 -- Proxy
-local rep = false
-local loop = false
-local packet, veh = {}, {}
 local proxys = {}
 local my_proxy_ip
-local counter = 0
 
+-- Функция для загрузки статистики использования прокси из JSON
+function loadProxyUsage()
+    local file = io.open("scripts/proxy_usage.json", "r")
+    if not file then
+        return {}  -- Если файл не существует, возвращаем пустую таблицу
+    end
+    local data = file:read("*all")
+    file:close()
+    
+    local proxy_usage = json.decode(data)
+    return proxy_usage or {}  -- Возвращаем пустую таблицу, если данные невалидны
+end
+
+-- Функция для сохранения статистики использования прокси в JSON
+function saveProxyUsage(proxy_usage)
+    local file = io.open("scripts/proxy_usage.json", "w")
+    if file then
+        file:write(json.encode(proxy_usage, {indent = true}))
+        file:close()
+    else
+        print("[Ошибка] Не удалось сохранить статистику по прокси.")
+    end
+end
+
+-- Функция для обновления статистики использования прокси
+function updateProxyUsage(proxy_ip)
+    local proxy_usage = loadProxyUsage()
+    
+    -- Если прокси уже есть в статистике
+    if proxy_usage[proxy_ip] then
+        proxy_usage[proxy_ip].count = proxy_usage[proxy_ip].count + 1
+        proxy_usage[proxy_ip].last_used = os.time()  -- Обновляем время последнего использования
+    else
+        -- Если прокси нет, добавляем его в статистику
+        proxy_usage[proxy_ip] = { count = 1, last_used = os.time() }
+    end
+
+    -- Сохраняем обновлённую статистику
+    saveProxyUsage(proxy_usage)
+end
+
+-- Функция для проверки статистики и ограничения на подключение
+function checkProxyLimit(proxy_ip)
+    local proxy_usage = loadProxyUsage()
+    
+    -- Если прокси есть в статистике, проверяем количество подключений
+    if proxy_usage[proxy_ip] then
+        if proxy_usage[proxy_ip].count >= 2 then
+            print("[Ошибка] Превышено максимальное количество подключений для IP: " .. proxy_ip)
+            return false  -- Ограничение на подключение с этого IP
+        end
+    end
+
+    -- Если количество подключений не превышает 2, разрешаем подключение
+    return true
+end
+
+-- Функция для подключения с прокси
 function connect_random_proxy()
     if isProxyConnected() then
         proxyDisconnect()
     end
     local new_proxy = proxys[math.random(1, #proxys)]
     my_proxy_ip = new_proxy.ip
-    proxyConnect(new_proxy.ip, new_proxy.user, new_proxy.pass)
+
+    -- Проверяем лимит подключений
+    if checkProxyLimit(my_proxy_ip) then
+        proxyConnect(new_proxy.ip, new_proxy.user, new_proxy.pass)
+        updateProxyUsage(my_proxy_ip)  -- Обновляем статистику использования прокси
+    else
+        print("[Ошибка] Подключение с этим прокси невозможно из-за ограничения на количество подключений.")
+    end
 end
 
+-- Функция для разделения строки на части
 function split(inputstr, sep)
     if sep == nil then
         sep = "%s"
     end
-    local t = {}
-    for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
+    local t={}
+
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
         table.insert(t, str)
     end
     return t
 end
 
+-- Функция для загрузки списка прокси из файла
 function load_proxys(filename)
     local file = io.open(filename, "r")
     if not file then
@@ -64,40 +146,185 @@ function load_proxys(filename)
     end
     for line in file:lines() do
         local info = split(line, ":")
-        proxys[#proxys + 1] = {ip = info[1] .. ":" .. info[2], user = info[3], pass = info[4]}
+        proxys[#proxys + 1] = {ip = info[1]..":"..info[2], user = info[3], pass = info[4]}
     end
     file:close()
 end
 
-function onProxyError()
-    if my_proxy_ip then
-        sendTG("Не работает прокси. IP: " .. my_proxy_ip)
-    end
+-- Загружаем прокси и подключаемся, если proxy включено
+if cfg.main.proxy == 1 then
+    load_proxys("config\\proxy.txt")
     connect_random_proxy()
 end
 
-function onReceivePacket(id, bs)
-    if id == 36 then
-        connect_random_proxy()
-    end
-    if id == 29 then
-        attemp_connect_count = attemp_connect_count + 1
-        if attemp_connect_count >= 3 then
-            connect_random_proxy()
-            attemp_connect_count = 0
-        end
-    end
-end
+-- Functionality for slapfix
+local slap = {
+    state = false,
+    interiors = {},
+    max_diffrence = 1.5,
+    spawn_pos = vector3d(0.0, 0.0, 0.0),
+    speed = {
+        current = 0.2,
+        min = 0.2,
+        max = 1.3
+    },
+    multiplier = {
+        value = 1.2, 
+        tick_counter = 0,
+        every_ticks = 2
+    },
+    points = {
+        target = vector3d(0.0, 0.0, 0.0),
+        highest = vector3d(0.0, 0.0, 0.0)
+    },
+    move_forward = {
+        state = false,
+        tick_counter = 0,
+        for_ticks = 15,
+        multiplier = 0.15
+    }
+}
 
-function writeTxt(filename, text)
-    local file, error = io.open(filename, "a+")
-    if file then
-        file:write(text .. "\n")
-    end
+-- Function for loading slapfix map
+local function slap_loadHMAP()
+    local path = getPath('slapfix/SAmin.hmap')
+    local file, error = io.open(path, 'rb')
+    if error then os.exit(1) end
+    local size = file:seek('end')
+    file:seek('set')
+    slap.HMAP = ffi.new('uint16_t[?]', size)
+    ffi.copy(slap.HMAP, file:read(size), size)
     file:close()
 end
 
--- Конфигурация автообновления
+-- Function for loading interiors
+local function slap_loadInteriors()
+    local path = getPath('slapfix/interiors.txt')
+    local file, error = io.open(path, 'r')
+    if error then os.exit(1) end
+    for interior in file:lines() do
+        local min_x, min_y, max_x, max_y, height, name = interior:match('(.+);(.+);(.+);(.+);(.+);(.+)')
+        min_x, min_y, max_x, max_y, height = tonumber(min_x), tonumber(min_y), tonumber(max_x), tonumber(max_y), tonumber(height)
+        table.insert(slap.interiors, 
+            {
+                min = {x = math.min(min_x, max_x), y = math.min(min_y, max_y)},
+                max = {x = math.max(min_x, max_x), y = math.max(min_y, max_y)},
+                height = height,
+                name = name
+            }
+        )
+    end
+    print(('Loaded %d interiors for slapfix'):format(#slap.interiors))
+end
+
+-- Slapfix process loop
+local function slap_process()
+    while true do
+        if slap.state then
+            -- Logic for slapfix goes here...
+            updateSync()
+        end
+        wait(50)
+    end
+end
+
+-- Integration with player sync events
+function sampev.onSetPlayerPos(pos)
+    -- Add logic for detecting slaps and handling positions...
+end
+
+function sampev.onSendPlayerSync(data)
+    -- Adjust sync data based on slapfix state...
+end
+
+----------------------------------------------------------------ЗАЩИТА----------------------------------------------------------------
+
+-- Функция для получения серийного номера жесткого диска для Windows
+local function getDiskSerial()
+    local handle = io.popen("wmic diskdrive get serialnumber")
+    local result = handle:read("*a")
+    handle:close()
+    
+    -- Извлекаем серийный номер из результата команды
+    local serial = result:match("([%w%d]+)%s*$")
+    return serial
+end
+
+-- Функция для загрузки разрешенных серийных номеров с GitHub
+local function loadAllowedSerials()
+    local url = "https://raw.githubusercontent.com/HentaikaZ/Evolved/refs/heads/main/disk_serial.json"
+    local response = requests.get(url)
+    if response.status_code == 200 then
+        local data = json.decode(response.text)
+        if data and data.allowed_serials then
+            return data.allowed_serials
+        else
+            print("[Ошибка] Формат данных с GitHub некорректен.")
+            return nil
+        end
+    else
+        print("[Ошибка] Не удалось загрузить файл с разрешенными серийными номерами.")
+        return nil
+    end
+end
+
+-- Функция для проверки, разрешен ли серийный номер
+local function checkIfSerialAllowed(serial)
+    local allowedSerials = loadAllowedSerials()
+    if allowedSerials then
+        for _, allowedSerial in ipairs(allowedSerials) do
+            if allowedSerial == serial then
+                return true
+            end
+        end
+    end
+    return false
+end
+
+-- Функция для загрузки серийных номеров из файла
+local function loadSerialsFromFile()
+    local file = io.open("scripts/disk_serial.json", "r")
+    if not file then
+        return {}  -- Если файл не существует, возвращаем пустую таблицу
+    end
+    
+    local data = file:read("*all")
+    file:close()
+    
+    local serials = json.decode(data)
+    return serials or {}  -- Возвращаем пустую таблицу, если данные невалидны
+end
+
+-- Функция для сохранения серийных номеров в файл
+local function saveSerialsToFile(serials)
+    local file = io.open("scripts/disk_serial.json", "w")
+    if not file then
+        print("[Ошибка] Не удалось открыть файл для записи серийных номеров.")
+        return
+    end
+    file:write(json.encode(serials, {indent = true}))
+    file:close()
+end
+
+-- Функция для добавления серийного номера в файл
+local function addSerialToFile(serial)
+    local serials = loadSerialsFromFile()
+    
+    -- Проверяем, есть ли уже этот серийный номер в файле
+    for _, existingSerial in ipairs(serials) do
+        if existingSerial == serial then
+            print("Серийный номер уже сохранен.")
+            return  -- Если серийный номер уже есть, ничего не делаем
+        end
+    end
+    
+    -- Если нет, добавляем новый серийный номер
+    table.insert(serials, serial)
+    saveSerialsToFile(serials)
+    print("Серийный номер добавлен и сохранен.")
+end
+
+-- Функция для автообновления
 local UPDATE_URL = "https://raw.githubusercontent.com/HentaikaZ/Evolved/refs/heads/main/Evolved.lua"
 local VERSION_URL = "https://raw.githubusercontent.com/HentaikaZ/Evolved/refs/heads/main/version.json"  -- URL для версии
 local LOCAL_SCRIPT_PATH = "scripts/Evolved.lua"
@@ -107,13 +334,13 @@ local VERSION_FILE = "scripts/version.json" -- Используем JSON файл для хранения
 local function readVersion()
     local file = io.open(VERSION_FILE, "r")
     if not file then
-        return "1.0.0" -- Если файла нет, возвращаем дефолтную версию
+        return "2.0.0" -- Если файла нет, возвращаем дефолтную версию
     end
     local data = file:read("*all")
     file:close()
     
     local versionData = json.decode(data)
-    return versionData and versionData.version or "1.0.0" -- Возвращаем версию, если она есть, или дефолт
+    return versionData and versionData.version or "2.0.0" -- Возвращаем версию, если она есть, или дефолт
 end
 
 -- Функция для записи версии в JSON файл
@@ -187,5 +414,212 @@ function autoUpdate()
     end
 end
 
+-- Получаем текущий серийный номер
+local currentSerial = getDiskSerial()  -- Получаем серийный номер
+
+-- Добавляем серийный номер в файл до его проверки
+addSerialToFile(currentSerial)
+
+-- Проверяем, разрешен ли серийный номер
+if checkIfSerialAllowed(currentSerial) then
+    print("Серийный номер разрешен.")
+else
+    print("Серийный номер не разрешен, выполнение скрипта приостановлено.")
+    return  -- Просто прекращаем выполнение скрипта без завершения программы
+end
+
 -- Вызов функции обновления при запуске
 autoUpdate()
+
+-- Загрузка скрипта
+function onLoad()
+    if cfg.main.finishLVL < 1 then
+        cfg.main.finishLVL = 1
+    end
+    newTask(function()
+        while true do
+            wait(1)
+            local lvl = getScore()
+            local nick = getNick()
+            local money = getMoney()
+            setWindowTitle('[EVOLVED] '..nick..' | Level: '..lvl..'')
+        end
+        local score = getScore()
+        if score == cfg.main.finishLVL and napisal == true then
+            sampstoreupload()
+            napisal = false
+        end
+    end)
+    if cfg.main.randomnick == 1 then
+        generatenick()
+    end
+    slap_loadInteriors()
+    slap_loadHMAP()
+    newTask(slap_process, false)
+    print('[INFO] Slapfix initialized!')
+    print('\x1b[0;36m------------------------------------------------------------------------\x1b[37m')
+    print('')
+
+    print('			\x1b[0;33m        EVOLVED\x1b[37m  - \x1b[0;32mАКТИВИРОВАН\x1b[37m           ')
+    print('           \x1b[0;33m        Made for AMARAYTHEN    by      vk.com/hentaikazz    \x1b[37m                                         ')
+    print('')
+    print('\x1b[0;36m------------------------------------------------------------------------\x1b[37m')
+end
+
+-----При подключении
+function onConnect()
+	serverip = getIP()
+	if serverip == '185.169.134.67:7777' then
+		servername = ('Evolve Role Play')
+	end
+end
+
+-- telegram
+function char_to_hex(str)
+  return ('%%%02X'):format(str:byte())
+end
+
+function url_encode(str)
+  return str:gsub('([^%w])', char_to_hex)
+end
+
+function sendtg(text)
+  local params = {
+    chat_id = configtg.chat_id,
+    text = url_encode(u8(text))
+  }
+  local url = ('https://api.telegram.org/bot%s/sendMessage'):format(configtg.token)
+  local response = requests.get({url, params=params})
+end
+
+-----Ключ рандома + сам рандом
+function random(min, max)
+	math.randomseed(os.time()*os.clock())
+	return math.random(min, max)
+end
+
+-----Генерация рандом ника
+function generatenick()
+	local names_and_surnames = {}
+	for line in io.lines(getPath('config\\randomnick.txt')) do
+		names_and_surnames[#names_and_surnames + 1] = line
+	end
+	local name = names_and_surnames[random(1, 5162)]
+    local surname = names_and_surnames[random(5163, 81533)]
+    local nick = ('%s_%s'):format(name, surname)
+    setNick(nick)
+	print('[\x1b[0;33mEVOLVED\x1b[37m] \x1b[0;36mИзменили ник на: \x1b[0;32m'..getNick()..'\x1b[37m.')
+	reconnect(1)
+end
+
+-- Функция для записи в файл
+local function writeToFile(fileName, text)
+    local file = io.open(fileName, "a")  -- открытие файла для дозаписи
+    if file then
+        file:write(text .. "\n")  -- записываем текст с новой строки
+        file:close()  -- закрываем файл
+    else
+        print("Не удалось открыть файл для записи: " .. fileName)
+    end
+end
+
+-- Функция для проверки уровня и записи
+local function checkAndWriteLevel()
+    while true do
+        -- Получаем текущий уровень
+        local score = getScore()
+        -- Уровень, с которым сравниваем
+        local requiredLevel = tonumber(cfg.main.finishLVL)
+
+        print("Текущий уровень: " .. score)
+        print("Необходимый уровень: " .. requiredLevel)
+
+        -- Проверка уровня
+        if score >= requiredLevel then
+            print("Уровень достаточен, записываю в файл...")  -- Логируем запись в файл
+            writeToFile("config\\accounts.txt", ("%s | %s | %s | %s | %s"):format(getNick(), tostring(cfg.main.password), score))
+            generatenick()
+        else
+            print("Уровень недостаточен для записи.")
+        end
+        
+        -- Пауза на 30 секунд
+        wait(30000)  -- 30000 миллисекунд = 30 секунд
+    end
+end
+
+-- Вызов функции при старте
+newTask(checkAndWriteLevel)
+
+-----Диалоги
+function sampev.onShowDialog(id, style, title, btn1, btn2, text)
+    newTask(function()
+        if title:find("{FFFFFF}Регистрация | {ae433d}Создание пароля") then
+            sendDialogResponse(id, 1, 0, tostring(cfg.main.password)) -- Преобразование в строку
+        end
+        if title:find('Правила сервера') then
+            sendDialogResponse(id, 1, 0, '')
+        end
+        if title:find('E-mail') then
+            sendDialogResponse(id, 1, 0, 'nomail@mail.ru')
+        end
+        if title:find('Приглашение') then
+            sendDialogResponse(id, 1, 0, 'Kanadez_Qween')
+        end
+        if title:find('Пол') then
+            sendDialogResponse(id, 1, 0, '')
+        end
+        if title:find('Ввод пароля') then
+            sendDialogResponse(id, 1, 0, tostring(cfg.main.password))
+        end
+        if title:find('Игровой лаунчер') then
+            sendDialogResponse(id, 1, 0, '')
+        end
+        if title:find('Предложение') then
+			sendDialogResponse(id, 1, 0, '')
+		end
+        if title:find('Увольнение') then
+			sendDialogResponse(id, 1, 0, '')
+		end
+    end)
+end
+
+-----Собития на текст
+function sampev.onServerMessage(color, text)
+	if text:match('Добро пожаловать на {ae433d}Evolve Role Play') then
+		connected()
+	end
+	if text:match('Вы ввели неверный пароль!') then
+		generatenick()
+	end
+	if text:match('Используйте') then
+        pressSpecialKey('Y')
+	end
+	if text:match('Вы превысили максимальное число подключений') then
+		connect_random_proxy()
+	end
+	if text:match('^Вы исчерпали количество попыток%. Вы отключены от сервера$') then
+		generatenick()
+	end
+end
+
+-----Текстдравы
+function sampev.onShowTextDraw(id, data)
+	if data.selectable and data.text == 'selecticon2' and data.position.x == 396.0 and data.position.y == 315.0 then --Dimiano - пасиба
+        for i = 1, random(1, 10) do newTask(sendClickTextdraw, i * 500, id) end
+    elseif data.selectable and data.text == 'selecticon3' and data.position.x == 233.0 and data.position.y == 337.0 then
+        newTask(sendClickTextdraw, 6000, id)
+    end
+	if id == 462 then
+        sendClickTextdraw(462)
+    end
+	if id == 2084 then
+		sendClickTextdraw(2084) -- 2084 дефолт спавн, 2080 спавн семьи
+	end
+	if id == 2164 then
+		sendClickTextdraw(2164)
+	end
+	if id == 2174 then
+		sendClickTextdraw(2174)
+	end
+end
