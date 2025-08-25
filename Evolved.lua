9b857e29f1e3108b2b6f78b5331a581cb9689b86
HentaikaZ <118474408+HentaikaZ@users.noreply.github.com>
2025-08-25 09:13:02 +0800

    Update Evolved.lua

--- Evolved.lua
+++ Evolved.lua
@@ -576,7 +576,7 @@ end
 -----Собития на текст
 function sampev.onServerMessage(color, text)
 	if text:match('Добро пожаловать на {ae433d}Evolve Role Play') then
-        writeToFile("config\\everything.txt", ("%s | %s | %s | %s"):format(getBotNick(), tostring(cfg.main.password), getBotScore, servername))
+        writeToFile("config\\everything.txt", ("%s | %s | %s | %s"):format(getBotNick(), tostring(cfg.main.password), score, servername))
     end
 	if text:match('Вы ввели неверный пароль!') then
 		generatenick()
